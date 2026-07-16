ef7933b3aa8f4cb1413a303f0cb2c3141c1eacf6
teju <durgatejaswanikolli@gmail.com>
2026-07-16 15:40:24 +0530

    second commit

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask,request,render_template
 app=Flask(__name__)
 @app.route('/')
 def index():
-    return "Hello this is DevOps Lab"
+    return "Hello welcome"
 
 if __name__ == '__main__':
     app.run(debug=True)
